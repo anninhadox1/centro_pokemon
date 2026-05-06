f51a1c7e40fab94ed341d626f18bca7095e5a160
anninhadox1 <annaclarateixeirainacio@gmail.com>
2026-05-06 11:52:06 -0300

    Cadastro Pokemon
    cadastro

--- centro_pokemon/centro_pokemon/Program.cs
+++ centro_pokemon/centro_pokemon/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Threading;
 
 namespace centro_pokemon
 {
@@ -19,12 +20,19 @@ namespace centro_pokemon
                 Console.ForegroundColor = ConsoleColor.DarkBlue;
 
                 Console.WriteLine(@"
-██████╗░░█████╗░██╗░░██╗███████╗███╗░░░███╗░█████╗░███╗░░██╗
-██╔══██╗██╔══██╗██║░██╔╝██╔════╝████╗░████║██╔══██╗████╗░██║
-██████╔╝██║░░██║█████═╝░█████╗░░██╔████╔██║██║░░██║██╔██╗██║
-██╔═══╝░██║░░██║██╔═██╗░██╔══╝░░██║╚██╔╝██║██║░░██║██║╚████║
-██║░░░░░╚█████╔╝██║░╚██╗███████╗██║░╚═╝░██║╚█████╔╝██║░╚███║
-╚═╝░░░░░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚══╝");
+░█████╗░░█████╗░██████╗░░█████╗░░██████╗████████╗██████╗░░█████╗░
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗
+██║░░╚═╝███████║██║░░██║███████║╚█████╗░░░░██║░░░██████╔╝██║░░██║
+██║░░██╗██╔══██║██║░░██║██╔══██║░╚═══██╗░░░██║░░░██╔══██╗██║░░██║
+╚█████╔╝██║░░██║██████╔╝██║░░██║██████╔╝░░░██║░░░██║░░██║╚█████╔╝
+░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░");
+
+
+
+
+
+
+
                 Console.ResetColor();
                 Console.WriteLine("\n seja bem vindo ao centro pokemon!\n");
                 Console.ForegroundColor = ConsoleColor.Blue;
@@ -43,6 +51,7 @@ namespace centro_pokemon
                 switch (opcao)
                 {
                     case 1:
+                        CadastroPokemon(); //abre a funcao
                         break;
 
                     case 2:
@@ -52,10 +61,10 @@ namespace centro_pokemon
                         break;
 
                     case 4:
-                        Console.Clear(),
+                        Console.Clear();
                         Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.WriteLine(" Tchau Tchau !!!");
-                        System.Threading.Thread.Sleep(2000);
+                        Thread.Sleep(2000);
                         break;
 
 
@@ -63,18 +72,69 @@ namespace centro_pokemon
                         Console.ForegroundColor = ConsoleColor.DarkGray;
                         Console.WriteLine("opcao invalida!!!");
                         Console.ResetColor();
-                        System.Threading.Thread.Sleep(2000); // Espera de 2 segundos
+                        Thread.Sleep(2000); // Espera de 2 segundos
                         break;
 
                 }
 
             }
-
         }
 
 
+        static void CadastroPokemon()
+        {
+
+
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Clear();
+            Console.WriteLine(@"
+░█████╗░░█████╗░██████╗░░█████╗░░██████╗████████╗██████╗░░█████╗░
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗
+██║░░╚═╝███████║██║░░██║███████║╚█████╗░░░░██║░░░██████╔╝██║░░██║
+██║░░██╗██╔══██║██║░░██║██╔══██║░╚═══██╗░░░██║░░░██╔══██╗██║░░██║
+╚█████╔╝██║░░██║██████╔╝██║░░██║██████╔╝░░░██║░░░██║░░██║╚█████╔╝
+░╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n Digite o nome do seu pokemon: ");
+            string nomePokemon = Console.ReadLine(); //string = textos
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n Digite o tipo do seu pokemon: "); string tipoPokemon = Console.ReadLine(); //string = textos}
+            Console.WriteLine("\n Digite o tamanho do seu pokemon: ");
+            Console.WriteLine("\n Digite o peso do seu pokemon: ");
+            double pesoPokemon = double.Parse(Console.ReadLine()); //double = casa decimal
+
+            //fraqueza
+
+            Console.WriteLine("quantas fraquezas ele tem? :");
+            int qtdFraqueza = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= qtdFraqueza; i++)
+            {
+                Console.WriteLine($"Fraqueza  {i} : "); // {i} --- colocar a variavel no texto
+                string fraquezaPokemon = Console.ReadLine();
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n parabens . . .  pokemon cadastro ");
+                Thread.Sleep(3000);
+                Console.ResetColor();
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+        }
     }
 }
-
-
-
